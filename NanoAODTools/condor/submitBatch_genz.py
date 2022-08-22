8c0bb45d990930e74c55ca771c1a51246ff83812
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2022-08-22 10:56:31 -0500

    Add EXO UL-LFVZ config to Gen-Z study

--- NanoAODTools/condor/submitBatch_genz.py
+++ NanoAODTools/condor/submitBatch_genz.py
@@ -35,19 +35,19 @@ nEvtPerJob = 2
 samplesDict['2016_z'] = [
     bm.JobConfig(
         dataset='/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISummer16NanoAODv7-PUMoriond17_Nano02Apr2020_102X_mcRun2_asymptotic_v8_ext2-v1/NANOAODSIM',
-        nEvtPerJobIn1e6=nEvtPerJob, year="2016", isData=False, suffix='GenZAnalysis_DY50-amc_2016'),
+        nEvtPerJobIn1e6=nEvtPerJob, year="2016", isData=False, suffix='GenZAnalysis_DY50_2016'),
 ]
 
 samplesDict['2017_z'] = [
     bm.JobConfig(
         dataset='/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17NanoAODv7-PU2017_12Apr2018_Nano02Apr2020_102X_mc2017_realistic_v8_ext3-v1/NANOAODSIM',
-        nEvtPerJobIn1e6=nEvtPerJob, year="2017", isData=False, suffix='GenZAnalysis_DY50-ext_2017'),    
+        nEvtPerJobIn1e6=nEvtPerJob, year="2017", isData=False, suffix='GenZAnalysis_DY50_2017'),    
 ]
 
 samplesDict['2018_z'] = [
     bm.JobConfig(
         dataset='/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIAutumn18NanoAODv7-Nano02Apr2020_102X_upgrade2018_realistic_v21_ext2-v1/NANOAODSIM',
-        nEvtPerJobIn1e6=nEvtPerJob, year="2018", isData=False, suffix='GenZAnalysis_DY50-amc_2018'),
+        nEvtPerJobIn1e6=nEvtPerJob, year="2018", isData=False, suffix='GenZAnalysis_DY50_2018'),
 ]
 
 #################################################
@@ -58,24 +58,30 @@ samplesDict['2018_z'] = [
 
 nEvtSigPerJob = 0.006 #only 125 events per file, 40k-80k per dataset, so 5-10 jobs per signal
 samplesDict['2016_signal'] = [
+    #original samples
     bm.JobConfig( 
-        dataset='/LFVAnalysis_ZEMu_2016_8028V1/pellicci-LFVAnalysis_NANOAOD_10222V1-07f11a9f6612f8436de459459102e3f2/USER',
+        dataset='/ZEMuAnalysis_2016_8028V1/pellicci-ZEMuAnalysis_NANOAOD_10218V1-b1c578360797952dfc156561d5f36519/USER',
         nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZEMu_2016', inputDBS="phys03"),
     bm.JobConfig( 
-        dataset='/LFVAnalysis_ZETau_2016_8028V1/pellicci-LFVAnalysis_NANOAOD_10222V1-07f11a9f6612f8436de459459102e3f2/USER',
+        dataset='/LFVAnalysis_ZETau_2016_8028V1/mimacken-LFVAnalysis_NANOAOD_8028V1-d11e799790792310589ef5ee63b17d7a/USER',
         nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZETau_2016', inputDBS="phys03"),
     bm.JobConfig( 
-        dataset='/LFVAnalysis_ZMuTau_2016_8028V1/pellicci-LFVAnalysis_NANOAOD_10222V1-07f11a9f6612f8436de459459102e3f2/USER',
+        dataset='/LFVAnalysis_ZMuTau_2016_8028V1/mimacken-LFVAnalysis_NANOAOD_8028V1-d11e799790792310589ef5ee63b17d7a/USER',
         nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZMuTau_2016', inputDBS="phys03"),
-    # bm.JobConfig( 
-    #     dataset='/ZEMuAnalysis_2016_8028V1/pellicci-ZEMuAnalysis_NANOAOD_10218V1-b1c578360797952dfc156561d5f36519/USER',
-    #     nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZEMu_2016', inputDBS="phys03"),
-    # bm.JobConfig( 
-    #     dataset='/LFVAnalysis_ZETau_2016_8028V1/mimacken-LFVAnalysis_NANOAOD_8028V1-d11e799790792310589ef5ee63b17d7a/USER',
-    #     nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZETau_2016', inputDBS="phys03"),
-    # bm.JobConfig( 
-    #     dataset='/LFVAnalysis_ZMuTau_2016_8028V1/mimacken-LFVAnalysis_NANOAOD_8028V1-d11e799790792310589ef5ee63b17d7a/USER',
-    #     nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZMuTau_2016', inputDBS="phys03"),
+    #v2 samples using AMC@NLO
+    bm.JobConfig( 
+        dataset='/LFVAnalysis_ZEMu_2016_8028V1/pellicci-LFVAnalysis_NANOAOD_10222V1-07f11a9f6612f8436de459459102e3f2/USER',
+        nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZEMu-v2_2016', inputDBS="phys03"),
+    bm.JobConfig( 
+        dataset='/LFVAnalysis_ZETau_2016_8028V1/pellicci-LFVAnalysis_NANOAOD_10222V1-07f11a9f6612f8436de459459102e3f2/USER',
+        nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZETau-v2_2016', inputDBS="phys03"),
+    bm.JobConfig( 
+        dataset='/LFVAnalysis_ZMuTau_2016_8028V1/pellicci-LFVAnalysis_NANOAOD_10222V1-07f11a9f6612f8436de459459102e3f2/USER',
+        nEvtPerJobIn1e6=nEvtSigPerJob, year="2016", isData=False, suffix='GenZAnalysis_ZMuTau-v2_2016', inputDBS="phys03"),
+    #EXO group UL sample
+    bm.JobConfig( 
+        dataset='/LFV_ZToLL_TuneCP5_13TeV-madgraph-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM',
+        nEvtPerJobIn1e6=0.5, year="2016", isData=False, suffix='GenZAnalysis_UL-LFVZ_2016'),
 ]
 
 samplesDict['2017_signal'] = [
