7d7a14ff43db4e6ee759ff7735d1243daaafcb8a
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2022-09-20 11:01:21 -0500

    Checkout analyzer script and module updates

---
+++ NanoAODTools/python/analyzers/LFVAnalyzer.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.LeptonSkimmer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.HTSkimmer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.JetSkimmer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.JetLepCleaner import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.SelectionFilter import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.GenCount import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.GenLepCount import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.GenAnalyzer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.GenZllAnalyzer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.GenRecoMatcher import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.TriggerAnalyzer import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.LeptonPairCreator import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.CUmodules.FunctionWrapper import *
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import *
+
+
+from importlib import import_module
+import os
+import sys
+import math
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+#read in command line arguments
+#Example: python <Analyzer> <input file> <data/MC/Embedded> <year>
+
+
+inputFile = sys.argv[1].split(',') #[ sys.argv[1] ]
+isData    =   sys.argv[2]
+year      =   sys.argv[3]
+
+prefetch  = False
+
+# inputFile = [
+#     "root://cms-xrd-global.cern.ch//store/user/pellicci/ZEMuAnalysis_2016_8028V1/ZEMuAnalysis_NANOAOD_10218V1/200211_105742/0000/ZEMuAnalysis_pythia8_NANOAOD_2016_1.root",\
+#     "root://cms-xrd-global.cern.ch//store/user/pellicci/ZEMuAnalysis_2016_8028V1/ZEMuAnalysis_NANOAOD_10218V1/200211_105742/0000/ZEMuAnalysis_pythia8_NANOAOD_2016_2.root",\
+# ]
+# inputFile = [
+#     "condor/tmp_data/ZEMu_2016_01.root", \
+#     "condor/tmp_data/ZEMu_2016_02.root", \
+# ]
+
+if isData not in ["data", "MC", "Embedded"]:
+   print "Unknown data flag %s" % (isData)
+   exit()
+   
+if year not in ["2016", "2017", "2018"]:
+   print "Unknown year %s" % (year)
+   exit()
+   
+#debug configuration options
+maxEntries=None #deactivate(use all evts): None
+firstEntry=0 #0 to start at the first event
+
+# branches to read in / write out
+branchsel_in  ="python/postprocessing/run/keep_and_drop_in.txt"
+branchsel_out ="python/postprocessing/run/keep_and_drop_out.txt"
+
+# filter out untriggered events or with leading lepton below the trigger threshold
+if year == "2016":
+   TriggerCuts="((HLT_IsoMu24 && nMuon > 0) || (HLT_Ele27_WPTight_Gsf && nElectron > 0))"
+elif year == "2017":
+   TriggerCuts="(HLT_IsoMu27 && nMuon > 0) || (HLT_Ele32_WPTight_Gsf_L1DoubleEG && nElectron > 0)"
+elif year == "2018":
+   TriggerCuts="(HLT_IsoMu24 && nMuon > 0) || (HLT_Ele32_WPTight_Gsf && nElectron > 0)"
+
+# TriggerCuts = None
+print "Trigger cuts:", TriggerCuts
+
+#Base lepton selection
+MuonSelection     = lambda l : l.pt>10 and math.fabs(l.eta)<2.2 and l.mediumId and l.pfRelIso04_all < 0.5
+ElectronSelection = lambda l : l.pt>10 and math.fabs(l.eta)<2.2 and l.mvaFall17V2noIso_WP90 and l.pfRelIso03_all < 0.5
+TauSelection      = lambda l : l.pt>20 and math.fabs(l.eta)<2.2 and l.idDeepTau2017v2p1VSmu > 10 and l.idDeepTau2017v2p1VSe > 10 and l.idDeepTau2017v2p1VSjet > 5 and l.idDecayModeNewDMs
+JetSelection      = lambda l : l.pt>20 and math.fabs(l.eta)<3.0 and l.puId>-1 and l.jetId>1 
+MaxMass = -1 # no cut
+MinMass = 50
+MinDeltaR = 0.3 # delta R between the leptons
+
+#configure the modules
+modules=[]
+GenCounter=GenCount()
+modules.append(GenCounter)
+
+#prefire probability, before jet/photon/electron collection is skimmed
+if isData == "MC": #only do on MC, 2016 and 2017
+   if year == "2016":
+      PrefireCorr = PrefCorr(jetroot="L1prefiring_jetpt_2016BtoH.root",
+                             jetmapname="L1prefiring_jetpt_2016BtoH",
+                             photonroot="L1prefiring_photonpt_2016BtoH.root",
+                             photonmapname="L1prefiring_photonpt_2016BtoH"
+      )
+      modules.append(PrefireCorr)
+   elif year == "2017":
+      PrefireCorr = PrefCorr(jetroot="L1prefiring_jetpt_2017BtoF.root",
+                             jetmapname="L1prefiring_jetpt_2017BtoF",
+                             photonroot="L1prefiring_photonpt_2017BtoF.root",
+                             photonmapname="L1prefiring_photonpt_2017BtoF"
+      )
+      modules.append(PrefireCorr)      
+   
+
+#lepton selection
+MuonSelector= LeptonSkimmer(
+   LepFlavour='Muon',
+   Selection=MuonSelection,
+   Veto=None,
+   minNlep=-1,
+   maxNlep=-1,
+   verbose=False
+)
+modules.append(MuonSelector)
+ElectronSelector= LeptonSkimmer(
+   LepFlavour='Electron',
+   Selection=ElectronSelection,
+   Veto=None,
+   minNlep=-1,
+   maxNlep=-1,
+   verbose=False
+)
+modules.append(ElectronSelector)
+
+# TriggerSelector = TriggerAnalyzer(
+#                    particlePdgId=13,
+#                    triggerBits=[1,10],
+#                    branchNames=["IsoMu24","Mu50"],
+#                    recoCollection="Muon",
+#                    maxDR=0.03,
+#                    maxRelDpt=10.
+                  
+#                    )
+# modules.append(TriggerSelector)
+
+TauSelector= LeptonSkimmer(
+   LepFlavour='Tau',
+   Selection=TauSelection,
+   Veto=None,
+   minNlep=-1,
+   maxNlep=-1,
+   verbose=False
+)
+modules.append(TauSelector)
+
+TauMuonCleaner=JetLepCleaner( 
+   Lepton='Muon',
+   Jet='Tau',
+   dRJet=0.3,
+   RemoveOverlappingJets=True, 
+   RemoveOverlappingLeptons=False
+)
+modules.append(TauMuonCleaner)   
+
+TauElectronCleaner=JetLepCleaner(
+   Lepton='Electron',
+   Jet='Tau',
+   dRJet=0.3,
+   RemoveOverlappingJets=True, 
+   RemoveOverlappingLeptons=False
+)
+modules.append(TauElectronCleaner)
+
+#filter events by final state selection
+Selection= SelectionFilter(year=year,
+                           min_mass = MinMass,
+                           max_mass = MaxMass,
+                           min_dr = MinDeltaR,
+                           verbose=0)
+modules.append(Selection)
+
+#Add additional object cleaning
+
+#FIXME: Add each year b-tag WP cuts
+JetSelector=JetSkimmer( 
+   BtagWPs=[0.1274, 0.4229, 0.7813 ], 
+   nGoodJetMin=-1, 
+   nBJetMax=20 , 
+   Selection=JetSelection,
+   Veto=None
+)
+modules.append(JetSelector)
+
+JetMuonCleaner=JetLepCleaner( 
+   Lepton='Muon',
+   Jet='Jet',
+   dRJet=0.3,
+   RemoveOverlappingJets=True, 
+   RemoveOverlappingLeptons=False
+)
+modules.append(JetMuonCleaner)   
+
+JetElectronCleaner=JetLepCleaner(
+   Lepton='Electron',
+   Jet='Jet',
+   dRJet=0.3,
+   RemoveOverlappingJets=True, 
+   RemoveOverlappingLeptons=False
+)
+modules.append(JetElectronCleaner)
+
+JetTauCleaner=JetLepCleaner( 
+   Lepton='Tau',
+   Jet='Jet',
+   dRJet=0.3,
+   RemoveOverlappingJets=True, 
+   RemoveOverlappingLeptons=False
+)
+modules.append(JetTauCleaner)   
+
+HTCalculator= HTSkimmer(
+   minJetPt=20,
+   minJetEta=3.0, #FIXME: Should be max jet eta I believe
+   minJetPUid=-1,
+   minHT=-1,
+   collection="Jet",
+   HTname="HT"
+)
+modules.append(HTCalculator)
+
+if not isData == "data":
+   ZllBuilder=GenZllAnalyzer(
+      variables=['pt','eta','phi','mass','pdgId'],
+      motherName='GenZll',
+      skip=False,
+      verbose=-1
+   )
+   modules.append(ZllBuilder)
+   # RecoElectronMatcher=GenRecoMatcher(
+   #                genParticles=['GenElectron'],
+   #                recoCollections=['Electron'],
+   #                maxDR=0.1
+   #                )
+   # modules.append(RecoElectronMatcher)
+   # RecoMuonMatcher=GenRecoMatcher(
+   #                genParticles=['GenMuon'],
+   #                recoCollections=['Muon'],
+   #                maxDR=0.1
+   #                )
+   # modules.append(RecoMuonMatcher)
+
+#record number of generator-level primary(-ish) leptons in the event
+GenTauCount= GenLepCount(Lepton="Tau")
+modules.append(GenTauCount)
+
+GenMuonCount= GenLepCount(Lepton="Muon")
+modules.append(GenMuonCount)
+
+GenElectronCount= GenLepCount(Lepton="Electron")
+modules.append(GenElectronCount)
+
+## FIXME: add back gen-reco matching
+# if not isData == "data":
+#    TauToEBuilder=GenAnalyzer(
+#       decay='15->11,-12,16',
+#       motherName='GenTauToE',
+#       daughterNames=['GenTauElectron','GenNeuE','GenNeuTau'],
+#       variables=['pt','eta','phi','pdgId'],
+#       grandmother="GenTau_Idx",
+#       conjugate=True,
+#       mother_has_antipart=True,
+#       daughter_has_antipart=[True,True,True],
+#       skip=False,
+#    )
+#    modules.append(TauToEBuilder)
+#    TauToMuBuilder=GenAnalyzer(
+#       decay='15->13,-14,16',
+#       motherName='GenTauToMuon',
+#       daughterNames=['GenTauMuon','GenNeuMu','GenNeuTau'],
+#       variables=['pt','eta','phi','pdgId'],
+#       grandmother="GenTau_Idx",
+#       conjugate=True,
+#       mother_has_antipart=True,
+#       daughter_has_antipart=[True,True,True],
+#       skip=False,
+#    )
+#    modules.append(TauToMuBuilder)
+
+
+
+
+#configure the pileup module and the json file filtering
+if isData == "MC":
+   if year == "2016":
+      modules.append(puAutoWeight_2016())
+   elif year == "2017":
+      modules.append(puAutoWeight_2017())
+   elif year == "2018":
+      modules.append(puAutoWeight_2018())
+   jsonFile=None
+else: #data/embedding
+   if year == "2016" :
+      jsonFile="json/Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt"
+   elif year == "2017":
+      jsonFile="json/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"
+   else:
+      jsonFile="json/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"
+
+p=PostProcessor(".", inputFile, cut = TriggerCuts, modules = modules, branchsel = branchsel_in, outputbranchsel = branchsel_out,
+                provenance = True, fwkJobReport = True, jsonInput = jsonFile, maxEntries = maxEntries, firstEntry = firstEntry)
+
+###############RUN here######################
+p.run()
+print "done"
+
+################################# options #############################
+#class PostProcessor:
+# outputDir, inputFiles, cut=None, branchsel=None, modules=[],compression="LZMA:9", friend=False, postfix=None, jsonInput=None,noOut=False, justcount=False, provenance=False, haddFileName=None,fwkJobReport=False, histFileName=None, histDirName=None, outputbranchsel=None, maxEntries=None, firstEntry=0, prefetch=False, longTermCache=False
+
---
+++ NanoAODTools/python/postprocessing/modules/CUmodules/SelectionFilter.py
@@ -0,0 +1,211 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Object
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+import ROOT
+import os
+import numpy as np
+import itertools
+import math
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+_rootLeafType2rootBranchType = {
+    'UChar_t': 'b', 'Char_t': 'B', 'UInt_t': 'i', 'Int_t': 'I', 'Float_t': 'F',
+    'Double_t': 'D', 'ULong64_t': 'l', 'Long64_t': 'L', 'Bool_t': 'O'}
+
+
+class SelectionFilter(Module):
+    def __init__(self, year, min_mass=50, max_mass=-1, min_dr=0.3, verbose=0):
+        self.year = year
+        self.min_mass = min_mass
+        self.max_mass = max_mass
+        self.min_dr = min_dr
+        self.verbose = verbose
+        self.seen = 0
+        self.mutau = [0,0]
+        self.etau = [0,0]
+        self.emu = [0,0]
+        self.mumu = [0,0]
+        self.ee = [0,0]
+        pass
+
+    def beginJob(self):
+        pass
+
+    def endJob(self):
+        pass
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+        self.out.branch("SelectionFilter_MuTau", 'O')
+        self.out.branch("SelectionFilter_ETau" , 'O')
+        self.out.branch("SelectionFilter_EMu"  , 'O')
+        self.out.branch("SelectionFilter_MuMu" , 'O')
+        self.out.branch("SelectionFilter_EE"   , 'O')
+        pass
+ 
+
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        print "SelectionFilter: Saw %i events: N(mutau) = %i; N(etau) = %i; N(emu) = %i; N(mumu) = %i; N(ee) = %i" % (self.seen, self.mutau[0],
+                                                                                                                      self.etau[0], self.emu[0],
+                                                                                                                      self.mumu[0], self.ee[0])
+        print "SelectionFilter: Passing tight IDs: N(mutau) = %i; N(etau) = %i; N(emu) = %i; N(mumu) = %i; N(ee) = %i" % (self.mutau[1],
+                                                                                                                          self.etau[1], self.emu[1],
+                                                                                                                          self.mumu[1], self.ee[1])
+        pass
+
+
+    def analyze(self, event):
+        """process event, return True (go to next module) or False (fail, go to next event)"""
+        self.seen = self.seen + 1
+
+        electrons  = Collection(event, "Electron")
+        muons      = Collection(event, "Muon"    )
+        taus       = Collection(event, "Tau"     )
+        HLT        = Object    (event, "HLT"     )
+
+        nElectrons = len(electrons)
+        nMuons     = len(muons    )
+        nTaus      = len(taus     )
+
+        if self.verbose > 1:
+            print "SelectionFilter Event %8i:\n N(e) = %i; N(mu) = %i; N(tau) = %i" % (self.seen, nElectrons, nMuons, nTaus)
+
+        #Count the selected leptons in the event to determine possible selections
+        mutau = nElectrons == 0 and nMuons     == 1 and nTaus  == 1
+        etau  = nElectrons == 1 and nMuons     == 0 and nTaus  == 1
+        emu   = nElectrons == 1 and nMuons     == 1
+        mumu  = nElectrons <  2 and nMuons     == 2
+        ee    = nElectrons == 2 and nMuons      < 2
+
+        if (mutau + etau + emu + mumu + ee) > 1:
+            print "SelectionFilter:: Event %8i: Error! More than one selection passed: N(e) = %i; N(mu) = %i; N(tau) = %i" % (self.seen, nElectrons, nMuons, nTaus)
+            #Enforcing exclusive categories
+            if (mutau and etau) or emu:
+                mutau = False
+                etau  = False
+            if (mumu and ee) or mutau or etau or emu:
+                mumu  = False
+                ee    = False
+
+        if self.verbose > 1:
+            print " Event survived trigger check"
+            print " Base     selection: mutau = %i; etau = %i; emu = %i; mumu = %i; ee = %i" % (mutau, etau, emu, mumu, ee)
+
+        if not (mutau or etau or emu or mumu or ee):
+            return False
+
+        #Check triggers
+        electronTriggered = False
+        muonTriggered     = False
+        elec_trig_pt      = 33.
+        muon_trig_pt      = 25.
+
+        #FIXME: Should we consider the Mu50 trigger?
+        muonHighTriggered = False #HLT.Mu50
+        if self.year == "2016":
+            electronTriggered = HLT.Ele27_WPTight_Gsf
+            muonTriggered     = HLT.IsoMu24
+            elec_trig_pt = 28.
+        elif self.year == "2017":
+            electronTriggered = HLT.Ele32_WPTight_Gsf_L1DoubleEG
+            muonTriggered     = HLT.IsoMu27
+            muon_trig_pt = 28.
+        elif self.year == "2018":
+            electronTriggered = HLT.Ele32_WPTight_Gsf
+            muonTriggered     = HLT.IsoMu24
+
+        if self.verbose > 1:
+            print " Muon triggered =", muonTriggered, "(low) and", muonHighTriggered, "(high); Electron triggered =", electronTriggered
+
+        #initial trigger filter
+        if not (electronTriggered or muonTriggered or muonHighTriggered):
+            return False
+
+        #Finally apply some additional event filtering
+        if mutau:
+            lep1 = muons[0]
+            lep2 = taus[0]
+            lep1_fl = 13
+            lep2_fl = 15
+        elif etau:
+            lep1 = electrons[0]
+            lep2 = taus[0]
+            lep1_fl = 11
+            lep2_fl = 15
+        elif emu:
+            lep1 = electrons[0]
+            lep2 = muons[0]
+            lep1_fl = 11
+            lep2_fl = 13
+        elif mumu:
+            lep1 = muons[0]
+            lep2 = muons[1]
+            lep1_fl = 13
+            lep2_fl = 13
+        elif ee:
+            lep1 = electrons[0]
+            lep2 = electrons[1]
+            lep1_fl = 11
+            lep2_fl = 11
+        else:
+            return False
+
+        #Apply some tighter selection IDs on the final leptons
+        min_mass      = self.min_mass
+        max_mass      = self.max_mass
+        min_delta_r   = self.min_dr
+
+        #Mass filter
+        lepm = (lep1.p4() + lep2.p4()).M()
+        if lepm < min_mass or (max_mass > min_mass and lepm > max_mass):
+            return False
+
+        #Delta R filter
+        if math.fabs(lep1.p4().DeltaR(lep2.p4())) < min_delta_r:
+            return False
+
+        #Next check the triggers
+        isTriggered = False
+        #check electron triggers
+        isTriggered = isTriggered or (abs(lep1_fl) == 11 and lep1.pt > elec_trig_pt and electronTriggered)
+        isTriggered = isTriggered or (abs(lep2_fl) == 11 and lep2.pt > elec_trig_pt and electronTriggered)
+        #check muon triggers
+        isTriggered = isTriggered or (abs(lep1_fl) == 13 and lep1.pt > muon_trig_pt and muonTriggered)
+        isTriggered = isTriggered or (abs(lep2_fl) == 13 and lep2.pt > muon_trig_pt and muonTriggered)
+        isTriggered = isTriggered or (abs(lep1_fl) == 13 and lep1.pt > 50 and muonHighTriggered)
+        isTriggered = isTriggered or (abs(lep2_fl) == 13 and lep2.pt > 50 and muonHighTriggered)
+
+        if not isTriggered:
+            return False
+
+        if self.verbose > 1:
+            print " Event survived trigger threshold filtering"
+
+
+        #Accept event
+
+        self.out.fillBranch("SelectionFilter_MuTau", mutau)
+        self.out.fillBranch("SelectionFilter_ETau" , etau)
+        self.out.fillBranch("SelectionFilter_EMu"  , emu)
+        self.out.fillBranch("SelectionFilter_MuMu" , mumu)
+        self.out.fillBranch("SelectionFilter_EE"   , ee)
+
+        if self.verbose:
+            print "SelectionFilter: Event %8i: mutau = %i; etau = %i; emu = %i; mumu = %i; ee = %i" % (self.seen, mutau, etau, emu, mumu, ee)
+
+        #Count the number of accepted events by category
+        self.mutau[0] = self.mutau[0] + mutau
+        self.etau[0]  = self.etau[0]  + etau
+        self.emu[0]   = self.emu[0]   + emu
+        self.mumu[0]  = self.mumu[0]  + mumu
+        self.ee[0]    = self.ee[0]    + ee
+
+        #Count the number of tight ID events for debugging/reference
+        self.mutau[1] = self.mutau[1] + (mutau and lep1.pfRelIso04_all < 0.15 and lep2.idDeepTau2017v2p1VSjet > 50)
+        self.mumu[1]  = self.mumu[1]  + (mumu  and lep1.pfRelIso04_all < 0.15 and lep2.pfRelIso04_all < 0.15)
+        self.etau[1]  = self.etau[1]  + (etau  and lep1.pfRelIso03_all < 0.15 and lep2.idDeepTau2017v2p1VSjet > 50)
+        self.emu[1]   = self.emu[1]   + (emu   and lep1.pfRelIso03_all < 0.15 and lep2.pfRelIso04_all < 0.15)
+        self.ee[1]    = self.ee[1]    + (ee    and lep1.pfRelIso03_all < 0.15 and lep2.pfRelIso03_all < 0.15)
+
+        return (mutau or etau or emu or mumu or ee)
